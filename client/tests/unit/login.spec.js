9fb157a0f846d4e82ef4431325c3807b7156d3fb
alu0101601830 <alu0101601830@ull.edu.es>
2023-01-12 12:17:06 +0000

    Corrección de tests y creacion del test de movie details

--- client/tests/unit/login.spec.js
+++ client/tests/unit/login.spec.js
@@ -1,18 +1,18 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import Login from '@/components/Login.vue'
+import Login from '../../src/components/LoginUser.vue'
 
 describe('Login.vue', () => {
     describe('when loaded', () => {
         it('has the required elements', () => {
             const wrapper = shallowMount(Login);
 
-            expect(wrapper.find('#title').exists()).toBe(true);
-            expect(wrapper.find('#title').text()).toBe('Login');
-            expect(wrapper.find('#usuario').exists()).toBe(true);
-            expect(wrapper.find('#contraseña').exists()).toBe(true);
-            expect(wrapper.find('#btn-login').exists()).toBe(true);
-            expect(wrapper.find('#btn-login').text()).toBe('Login');
+            expect(wrapper.find('#title').exists()).to.be.equal(true);
+            expect(wrapper.text()).to.include('Login')
+            expect(wrapper.find('#gmail').exists()).to.be.equal(true);
+            expect(wrapper.find('#contraseña').exists()).to.be.equal(true);
+            expect(wrapper.find('#btn-login').exists()).to.be.equal(true);
+            expect((wrapper.find('#btn-login')).text()).to.equal('Login');
 
 
           })
